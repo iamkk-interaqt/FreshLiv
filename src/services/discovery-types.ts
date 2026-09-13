f4d3c6647b3304a226d921d374e14ef56879ed68
iamkk-interaqt <iamkaruneshkant@gmail.com>
2026-09-14 02:48:46 +0530

    feat: define discovery result type

---
+++ src/services/discovery-types.ts
@@ -0,0 +1,10 @@
+export interface DairywalaDiscoveryResult {
+  id: string;
+  business_name: string;
+  contact_name: string | null;
+  locality: string | null;
+  city: string | null;
+  state: string | null;
+  postal_code: string | null;
+  status: string;
+}
